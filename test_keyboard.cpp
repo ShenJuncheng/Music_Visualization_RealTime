f9627fb6ab3305d01bdb97e81c77d575a218f677
testingforforwhat <2273664Z@student.gla.ac.uk>
2023-02-16 00:10:03 +0000

    test_led

--- test_keyboard.cpp
+++ test_keyboard.cpp
@@ -10,7 +10,7 @@ int main()
 {
     // 获取从键盘输入的数据
     char buf[1024] = {0};
-    printf("请输入数据：");
+    printf("please input data: ");
     char *str = fgets(buf, sizeof(buf) - 1, stdin); // fgets()是阻塞的
     printf("buf : %s\n", buf);
     printf("str : %s\n", str);
