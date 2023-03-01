6352e52ee714142c89ac5cf73187b6347d95efe2
testingforforwhat <2273664Z@student.gla.ac.uk>
2023-03-01 16:40:29 +0000

    arduino_test

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ wiringpi元件驱动
 
 
 task one；声音传感器的接收与控制
+
+linux_github  https://blog.csdn.net/sasafa/article/details/125699014
+版本检测  git --version
+git clone + ssh路径/https路径   #拷贝一份远程仓库代码到本地
+
---
+++ src/arduino_test.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 23194 on 2023/3/1.
+//
+
+//https://blog.csdn.net/weixin_64131855/article/details/123671846
+#include <stdio.h>
+#include <wiringPi.h>
+#include <pcf8591.h>
+#define makerobo_PCF 120
+int main （void）
+{
+int sound_value;
+wiringPiSetup ();
+pcf8591Setup (makerobo_PCE, Ox48);
+while(1)
+{
+
+sound_value = analogRead (makerobo_PCF + 0);
+printf("value: &d\n", sound_value);
+
+if (sound_value < 80){
+printf("Makerobo Voice In! \n");
+}
+delay(100);
+}
+return 0;
+}
+
