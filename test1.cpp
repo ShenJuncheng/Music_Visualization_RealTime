9bbc69c19e5e4d277e864389a2558d867a23e8f0
Jinfeng-Lyu <123096011+JinfengLyu@users.noreply.github.com>
2023-03-10 01:14:57 +0800

    the 5 has been inserted

--- test1.cpp
+++ test1.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "gtest/gtest.h"
+int add(int a,int b) {return a+b;}
+TEST(add,zero) {EXPECT_EQ(0,add(0,0)) <<"yes";}
+TEST(add,positive_number) {EXPECT_EQ(3,add(1,2));}
+TEST(add,negative_number) {EXPECT_EQ(-3,add(-1,-2));}
+int main() {::testing::InitGoogleTest();
+return RUN_ALL_TESTS();}
 
-int Factorial(int n) {
-int result = 1;
-for(int i=1; i<=n; i++)
-    {result*=i;}
-return result;
-}
+
