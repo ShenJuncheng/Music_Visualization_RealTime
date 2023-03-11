3fe1e49d224993077b7bf25bc6627fa90e99496a
testingforforwhat <2273664Z@student.gla.ac.uk>
2023-03-11 22:34:47 +0000

    pcf8591

--- src/arduino_test.c
+++ src/arduino_test.c
@@ -20,13 +20,13 @@ int main (void)
 {
     int sound_value;
     wiringPiSetup();
-    pcf8591Setup(makerobo_PCF, 0x48);
+    pcf8591Setup(makerobo_PCF, 0x48);  //0x48 是 pcf8591 的 I2C 设备地址; pcf8591 可以有 8 个不同的 I2C 地址，但默认为 0x48
     while(1)
     {
 
         sound_value = analogRead(makerobo_PCF + 0);
         printf("test \n");
-        printf("value: &d\n", sound_value);  //%d 输出的数据转化为十进制的一个数
+        printf("value: %d\n", sound_value);  //%d 输出的数据转化为十进制的一个数
         if (sound_value < 80){
             printf("Makerobo Voice In! \n");
         }
@@ -36,3 +36,4 @@ int main (void)
 }
 
 //https://baike.baidu.com/item/PCF8591/3916799?fr=aladdin
+//https://forums.raspberrypi.com/viewtopic.php?t=177559
