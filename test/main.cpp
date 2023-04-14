05d7a630643ead5ac65cd70c22b9528ff0620104
testingforforwhat <2273664Z@student.gla.ac.uk>
2023-04-14 19:03:18 +0100

    unit test #7

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,8 @@
 #include "gtest/gtest.h"
-#include "gtest_scene.h"
-#include "gtest_camera.h"
-#include "gtest_cnnprocessor.h"
-#include "gtest_blockingqueue.h"
-#include "gtest_preprocessor.h"
-#include "gtest_signapseutils.h"
+#include "gtest_AudioSource.h"
+#include "gtest_LEDThread.h"
+#include "gtest_SetDevice.h"
+#include "gtest_WS2812B.h"
 //#include ""
 
 
